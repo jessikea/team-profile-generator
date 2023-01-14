3d76ef7d00800b0f1338105123875eb46a4eeef5
jessikea <jessikea@jtran.git>
2023-01-14 16:10:04 -0500

    added intern fixed build team function

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMembers= [];
+const idArray =[];
+
 // Informing user of usage of generator
 
 console.log(
@@ -17,81 +20,71 @@ console.log(
 );
 
 function startApp(){
-
-    function buildTeam() {
-        if (!fs.existsSync(OUTPUT_DIR)) {
-          fs.mkdirSync(OUTPUT_DIR);
-        }
-        fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
-      }
-    
-      createManager();
-    }
     function createManager(){
         console.log("Time to build your team!");
         inquirer
-            .prompt(
-                [
-                {
-                    type: 'input',
-                    name: 'managerName',
-                    message: "What is the team manager's name?",
+            .prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: "What is the team manager's name?",
 
-                     // to check if name is at least one character 
-                    validate: (answer) => {
-                        if (answer !== '') {
+                 // to check if name is at least one character 
+                validate: (answer) => {
+                    if (answer !== '') {
                         return true;
-                      }
+                    }
                         return 'Please enter at least one character.';
-                    },
-                 },
+                }
+            },
 
-                { 
-                    type: "input",
-                    name: "managerId",
-                    message: "Please provide Team Manager's ID",
-                    // to check if ID is valid with only number characters 
-
-                    validate: (answer) => {
-                        const accept = answer.match(/^[1-9]\d*$/)
-
-                        if (accept) {
-                            return true;
-                        }
+            { 
+                type: "input",
+                name: "managerId",
+                message: "Please provide Team Manager's ID",
+                
+                  // to check if ID is valid with only number characters 
+                validate: (answer) => {
+                    const accept = answer.match(/^[1-9]\d*$/)
+                    if (accept) {
+                        return true;
+                    }
                         return "Please enter a valid ID number";
-                    },
-                },
-                { 
-                    type: 'input',
-                    name: 'managerEmail',
-                    message: "What is the team manager's email?",
+                }
+            },
+            
+            { 
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is the team manager's email?",
 
-                    // to check if there is a valid email with an @ symbol
-                    validate: (answer) => {
+                  // to check if there is a valid email with an @ symbol
+                validate: (answer) => {
                     const accept = answer.match(/\S+@\S+\.\S+/);
-                        if (accept) {
-                            return true;
+                     if (accept) {
+                        return true;
                         }
-                         return 'Please enter a valid email address.';
-                    },
-                },
+                        return 'Please enter a valid email address.';
+                }
+            },
 
-                {
-                    type: 'input',
-                    name: 'managerOfficeNumber',
-                    message: "What is the team manager's office number?",
+            {
+                type: 'input',
+                name: 'managerOfficeNumber',
+                message: "What is the team manager's office number?",
+                    
                     // to check if the team manager's office number constains numbers
-                    validate: (answer) => {
+                validate: (answer) => {
                     const accept = answer.match(/^[1-9]\d*$/);
-                        if (accept) {
-                            return true;
-                          }
-                          return 'Please enter a valid Office Number.';
-                    },
-                },
+                    if (accept) {
+                        return true;
+                    }
+                        return 'Please enter a valid Office Number.';
+                }
+            },
 
-                ])
-
+            ])
+                
             .then((answers) => {
                 const manager = new Manager(
                     answers.managerName,
@@ -106,68 +99,143 @@ function startApp(){
     }
     function addEngineer(){
         inquirer
-            .prompt(
-                [
-                {
-                    type: 'input',
-                    name: 'engineerName',
-                    message: "What is your engineer's name?",
+            .prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: "What is your engineer's name?",
     
                     // to check if name is at least one character 
+                validate: (answer) => {
+                    if (answer !== '') {
+                        return true;
+                    }
+                        return 'Please enter at least one character.';
+                }
+            },
+    
+            { 
+                type: "input",
+                name: "engineerId",
+                message: "Please provide your engineer's ID",
+                    
+                    // to check if ID is valid with only number characters 
+                 validate: (answer) => {
+                    const accept = answer.match(/^[1-9]\d*$/)
+                        if (accept) {
+                            return true;
+                        }
+                            return "Please enter a valid ID number";
+                }
+            },
+
+            { 
+                type: 'input',
+                name: 'engineerEmail',
+                message: "What is the engineer's email?",
+                    // to check if there is a valid email with an @ symbol
+                validate: (answer) => {
+                    const accept = answer.match(/\S+@\S+\.\S+/);
+                    if (accept) {
+                        return true;
+                        }
+                        return 'Please enter a valid email address.';
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'engineerGithub',
+                message: "What is your engineer's GitHub username?",
+                    
                     validate: (answer) => {
-                        if (answer !== '') {
+                    if (answer !== '') {
                         return true;
                         }
                         return 'Please enter at least one character.';
                     },
-                },
-    
-                { 
-                    type: "input",
-                    name: "engineerId",
-                    message: "Please provide your engineer's ID",
-                    
-                    // to check if ID is valid with only number characters 
-    
-                    validate: (answer) => {
-                        const accept = answer.match(/^[1-9]\d*$/)
-    
-                        if (accept) {
-                         return true;
-                        }
-                         return "Please enter a valid ID number";
-                    },
-                    },
-                { 
-                    type: 'input',
-                    name: 'engineerEmail',
-                    message: "What is the engineer's email?",
-    
-                    // to check if there is a valid email with an @ symbol
-                    validate: (answer) => {
-                        const accept = answer.match(/\S+@\S+\.\S+/);
-                        
-                        if (accept) {
-                         return true;
-                        }
-                         return 'Please enter a valid email address.';
-                    },
+            },
+            ])
+
+            .then((answers) => {
+                const engineer = new Engineer(
+                  answers.engineerName,
+                  answers.engineerId,
+                  answers.engineerEmail,
+                  answers.engineerGithub
+                );
+                teamMembers.push(engineer);
+                idArray.push(answers.engineerId);
+                createTeam();
+              });
+          }
+
+          function addIntern() {
+            inquirer
+              .prompt([
+                {
+                  type: 'input',
+                  name: 'internName',
+                  message: "What is your intern's name?",
+                  validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'Please enter at least one character.';
+                  },
                 },
                 {
-                    type: 'input',
-                    name: 'engineerGithub',
-                    message: "What is your engineer's GitHub username?",
-                    
-                    validate: (answer) => {
-                        if (answer !== '') {
-                         return true;
-                        }
-                         return 'Please enter at least one character.';
-                    },
+                  type: 'input',
+                  name: 'internId',
+                  message: "What is your intern's id?",
+                  validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                      if (idArray.includes(answer)) {
+                        return 'This ID is already taken. Please enter a valid ID number.';
+                      } else {
+                        return true;
+                      }
+                    }
+                    return 'Please enter a valid ID number.';
+                  },
                 },
-                
-                ])
-            }
+                {
+                  type: 'input',
+                  name: 'internEmail',
+                  message: "What is your intern's email?",
+                  validate: (answer) => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                      return true;
+                    }
+                    return 'Please enter a valid email address.';
+                  },
+                },
+                {
+                  type: 'input',
+                  name: 'internSchool',
+                  message: "What is your intern's school?",
+                  validate: (answer) => {
+                    if (answer !== '') {
+                      return true;
+                    }
+                    return 'Please enter at least one character.';
+                  },
+                },
+              ])
+              .then((answers) => {
+                const intern = new Intern(
+                  answers.internName,
+                  answers.internId,
+                  answers.internEmail,
+                  answers.internSchool
+                );
+                teamMembers.push(intern);
+                idArray.push(answers.internId);
+                createTeam();
+              });
+          }
     function createTeam() {
         inquirer
           .prompt([
@@ -195,6 +263,15 @@ function startApp(){
             }
           });
       }
+      function buildTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR);
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
+      }
+    
+      createManager();
+    }
 
 startApp()
 // Write code to use inquirer to gather information about the development team members,
